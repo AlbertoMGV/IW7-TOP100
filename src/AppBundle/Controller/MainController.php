5d2208a36751cc319037cc3fc87c08918dc49153
Alberto Miranda <alberto.miranda@opendeusto.es>
2018-11-14 00:45:44 +0100

    BigUpdate (see desc)
    -Added index cards
    -Base Almost finished(footer left)
    -Favicon Changed
    -Created methods in controler
    -Added IMGs
    -Added artists page
    -Added songs page
    -Added genres page

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -18,4 +18,34 @@ class MainController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+    /**
+     * @Route("/songs", name="songs")
+     */
+    public function songsAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('main/songs.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+    /**
+     * @Route("/artists", name="artists")
+     */
+    public function artistsAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('main/artists.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+    /**
+     * @Route("/genres", name="genres")
+     */
+    public function genresAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('main/genres.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
 }
