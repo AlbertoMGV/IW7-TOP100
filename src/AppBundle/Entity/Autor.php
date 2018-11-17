ee4cc254ecb4162db53f08c5bcde308ca89dd753
DESKTOP-JPKTNR3\ivans <ramon.sanchez68@gmail.com>
2018-11-17 17:54:22 +0100

    Añadidas entidades

---
+++ src/AppBundle/Entity/Autor.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\OneToMany;
+use Doctrine\ORM\Mapping\JoinColumn;
+
+/**
+ * Autor
+ *
+ * @ORM\Table(name="autor")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\AutorRepository")
+ */
+class Autor
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="apellidos", type="string", length=255)
+     */
+    private $apellidos;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="fecha_nacimiento", type="integer")
+     */
+    private $fechaNacimiento;
+
+    /**
+     * One product has many features. This is the inverse side.
+     * @OneToMany(targetEntity="Cancion", mappedBy="autor")
+     */
+
+    private $canciones;
+
+    public function __construct(){
+        $this->canciones = new ArrayCollection();
+    }
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre.
+     *
+     * @param string $nombre
+     *
+     * @return Autor
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre.
+     *
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set apellidos.
+     *
+     * @param string $apellidos
+     *
+     * @return Autor
+     */
+    public function setApellidos($apellidos)
+    {
+        $this->apellidos = $apellidos;
+
+        return $this;
+    }
+
+    /**
+     * Get apellidos.
+     *
+     * @return string
+     */
+    public function getApellidos()
+    {
+        return $this->apellidos;
+    }
+
+    /**
+     * Set fechaNacimiento.
+     *
+     * @param int $fechaNacimiento
+     *
+     * @return Autor
+     */
+    public function setFechaNacimiento($fechaNacimiento)
+    {
+        $this->fechaNacimiento = $fechaNacimiento;
+
+        return $this;
+    }
+
+    /**
+     * Get fechaNacimiento.
+     *
+     * @return int
+     */
+    public function getFechaNacimiento()
+    {
+        return $this->fechaNacimiento;
+    }
+
+    public function setCanciones($canciones = null){
+        $this->canciones = $canciones;
+
+        return $this;
+    }
+
+    public function getCanciones(){
+        return $this->canciones;
+    }
+
+
+
+    /**
+     * Add cancione.
+     *
+     * @param \AppBundle\Entity\Cancion $cancione
+     *
+     * @return Autor
+     */
+    public function addCancione(\AppBundle\Entity\Cancion $cancione)
+    {
+        $this->canciones[] = $cancione;
+
+        return $this;
+    }
+
+    /**
+     * Remove cancione.
+     *
+     * @param \AppBundle\Entity\Cancion $cancione
+     *
+     * @return boolean TRUE if this collection contained the specified element, FALSE otherwise.
+     */
+    public function removeCancione(\AppBundle\Entity\Cancion $cancione)
+    {
+        return $this->canciones->removeElement($cancione);
+    }
+}
---
+++ src/AppBundle/Entity/Cancion.php~
@@ -0,0 +1,161 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\ManyToOne;
+use Doctrine\ORM\Mapping\JoinColumn;
+
+
+/**
+ * Cancion
+ *
+ * @ORM\Table(name="cancion")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CancionRepository")
+ */
+class Cancion
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="duracion", type="integer")
+     */
+    private $duracion;
+
+    /**
+     *
+     * @ManyToOne(targetEntity="Autor", inversedBy="canciones")
+     * @JoinColumn(name="autor_id", referencedColumnName="id")
+     */
+    private $autor;
+
+     /**
+     *
+     * @ManyToOne(targetEntity="Genero", inversedBy="canciones")
+     * @JoinColumn(name="genero_id", referencedColumnName="id")
+     */
+    private $genero;
+
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre.
+     *
+     * @param string $nombre
+     *
+     * @return Cancion
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre.
+     *
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set duracion.
+     *
+     * @param int $duracion
+     *
+     * @return Cancion
+     */
+    public function setDuracion($duracion)
+    {
+        $this->duracion = $duracion;
+
+        return $this;
+    }
+
+    /**
+     * Get duracion.
+     *
+     * @return int
+     */
+    public function getDuracion()
+    {
+        return $this->duracion;
+    }
+
+    /**
+     * Set autor.
+     *
+     * @param \stdClass $autor
+     *
+     * @return Cancion
+     */
+    public function setAutor($autor)
+    {
+        $this->autor = $autor;
+
+        return $this;
+    }
+
+    /**
+     * Get autor.
+     *
+     * @return \stdClass
+     */
+    public function getAutor()
+    {
+        return $this->autor;
+    }
+
+    /**
+     * Set genero.
+     *
+     * @param \stdClass $genero
+     *
+     * @return Cancion
+     */
+    public function setGenero($genero)
+    {
+        $this->genero = $genero;
+
+        return $this;
+    }
+
+    /**
+     * Get genero.
+     *
+     * @return \stdClass
+     */
+    public function getGenero()
+    {
+        return $this->genero;
+    }
+}
