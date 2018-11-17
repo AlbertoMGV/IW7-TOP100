1cc51cf70f51a5b5c4c236d703480ee67ba1647f
DESKTOP-JPKTNR3\ivans <ramon.sanchez68@gmail.com>
2018-11-17 18:08:54 +0100

    A medias

---
+++ src/AppBundle/Controller/SongController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+use AppBundle\Entity\Genero;
+use AppBundle\Entity\Cancion;
+use AppBundle\Entity\Autor;
+
+
+class SongController extends Controller{
+
+    /**
+     * @Route("/songs/create", name=create_song)
+     */
+
+     public function createSong(){
+         $genre = new Genre();
+         $genre->setNombre("jazz");
+         $genre->setDescripcion("El jazz es un género musical nacido a finales del siglo XIX en los Estados Unidos, que se expandió de forma global a lo largo de todo el siglo XX.");
+         $autor = new Autor();
+         $autor->setNombre("Louis");
+         $autor->setApellidos("Armstrong");
+
+     }
+
+
+
+}
+
+
+?>
