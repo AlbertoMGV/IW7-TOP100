1202a9a2604ee58b8fb8a8b957da01eb2902f511
Alberto Miranda <alberto.miranda@opendeusto.es>
2018-12-13 17:03:36 +0100

    Firebase+Angular

--- E3B/js/agendaCtrl.js
+++ E3B/js/agendaCtrl.js
@@ -1,20 +1,43 @@
 app.controller("agendaCtrl", function ($scope) {
 
-  $scope.contactos = [
-    { name: 'Pablo Garaizar', email: 'garaizar@deusto.es', phone: '2512' }
-  ];
+    $scope.contactos = [];
+    $scope.query = "";
+    $scope.nuevo = { name: '', email: '', phone: '' };
 
-  $scope.nuevo = { name: '', email: '', phone: '' };
+    var ref = new Firebase('https://agendafirebase.firebaseio.com/contactos');
+
+
+
+    ref.on('child_added', function(snapshot) {
+        var contacto = snapshot.val();
+        var clave = snapshot.key();
+        $scope.contactos.push( { id: clave, name: contacto.name, email: contacto.email, phone: contacto.phone } );
+    });
 
   $scope.nuevoContacto = function () {
+ 
     if ($scope.nuevo.name !== '' && $scope.nuevo.email !== '' && $scope.nuevo.phone !== '') {
-      $scope.contactos.push(angular.copy($scope.nuevo));
-      $scope.nuevo = { name: '', email: '', phone: '' };
+
+      ref.push({
+        name: $scope.nuevo.name,
+        email: $scope.nuevo.email,
+        phone: $scope.nuevo.phone
+        }, function () { 
+        $scope.nuevo = { name: '', email: '', phone: '' };
+        }
+      );
     }
+
   };
 
-  $scope.borrarContacto = function (index) {
-    $scope.contactos.splice(index, 1);
+  $scope.borrarContacto = function (index, key) {
+
+    var contactoRef = new Firebase('https://agendafirebase.firebaseio.com/contactos/'+key);
+    contactoRef.remove(function () { 
+        $timeout(function () {
+           $scope.contactos.splice(index, 1);
+        }, 500); 
+    });
   };
 
-});   
+});
